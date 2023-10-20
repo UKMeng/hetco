9fa6b2c28de240ee3a6e44858fa03a158d5cb259
UKMeng <ukmengse@gmail.com>
2023-10-20 21:06:12 +0800

    ✨ feat(chapter 2): reading user input
    use std::io to read user input; use termion let terminal into raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::io::{self, stdout, Read};
+use termion::raw::IntoRawMode;
+
+fn to_ctrl_byte(c: char) -> u8 {
+    let byte = c as u8;
+    byte & 0b0001_1111
+}
+
+fn die(e: std::io::Error) {
+    panic!("{}", e);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _stdout = stdout().into_raw_mode().unwrap();
+
+    for b in io::stdin().bytes() {
+        // bytes() needs std::io::Read
+        match b {
+            Ok(b) => {
+                let c = b as char;
+                if c.is_control() {
+                    println!("{:?} \r", b);  // \r The carriage return moves the cursor back to the beginning of the current line
+                } else {
+                    println!("{:?} ({})\r", b, c);
+                }
+                if b == to_ctrl_byte('c') {
+                    break;
+                }
+            }
+            Err(err) => die(err),
+        }
+        
+    }
 }
