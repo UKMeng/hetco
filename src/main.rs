305146f1027ad5e1a105a8c947d1b6161c0100d2
UKMeng <ukmengse@gmail.com>
2023-10-21 10:34:53 +0800

    ✨ feat(chapter 4): apply line viewer

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 #![warn(clippy::all, clippy::pedantic)]
+mod document;
 mod editor;
+mod row;
 mod terminal;
-
+pub use document::Document;
 use editor::Editor;
 pub use terminal::Terminal;
+pub use row::Row;
 pub use editor::Position;
 // re-exporting the Terminal struct at the top level and make it reachable via crate::Terminal
 
